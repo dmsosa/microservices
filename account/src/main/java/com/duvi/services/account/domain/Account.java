516c1122163eead194dd11444652d50d4f8a6c66
Durian Sosa <dmsosa21@outlook.com>
2024-02-23 18:36:14 +0000

    Account und Gateway

---
+++ account/src/main/java/com/duvi/services/account/domain/Account.java
@@ -0,0 +1,4 @@
+package com.duvi.services.account.domain;
+
+public class Account {
+}
---
+++ account/src/test/java/com/duvi/services/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duvi.services.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
