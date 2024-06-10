b9640629030c30d99d233269256aa4d2c68ce2d9
Durian Sosa <dmsosa21@outlook.com>
2024-06-10 09:10:50 +0100

    Custom Select box

--- gateway/src/main/resources/static/js/main.js
+++ gateway/src/main/resources/static/js/main.js
@@ -2,8 +2,97 @@
 import { show } from './login.js';
 import { showModalToEdit, saveFromModalToItem, findWrapId, findInputChildren } from './dashboard.js';
 
+
+
+function createCustomSelects() {
+    var allSelects = $("select");
+    for (let i = 0; i < allSelects.length ; i++) {
+    var parent = allSelects[i].parentNode;
+    var wrapper = document.createElement("div");
+    wrapper.setAttribute("class", "custom-select");
+    parent.replaceChild(wrapper, allSelects[i]);
+    wrapper.appendChild(allSelects[i]);
+    
+}
+}
+//wrapping all selects in custom select divs
+
+function initializeCustomSelects() {
+    var customSelects, selectElement, optionsLenght, currentSelectedOption, optionsList;
+
+    customSelects = $(".custom-select");
+    for (let i = 0 ; i < customSelects.length ; i++) {
+        // create current selected item
+        selectElement = customSelects[i].getElementsByTagName("select")[0];
+        optionsLenght = selectElement.length;
+        currentSelectedOption = document.createElement("div");
+        currentSelectedOption.setAttribute("class", "select-selected");
+        currentSelectedOption.innerHTML = selectElement.options[selectElement.selectedIndex].innerHTML;
+        customSelects[i].appendChild(currentSelectedOption);
+        
+        optionsList = document.createElement("div");
+        optionsList.setAttribute("class", "select-items select-hide");
+        for (let j = 0 ; j < optionsLenght ; j++) {
+            createOptionElement(optionsList, selectElement, j);
+        }
+        customSelects[i].appendChild(optionsList);
+        currentSelectedOption.addEventListener("click", function(e) {
+            e.stopPropagation();
+            closeAllSelect(this);
+            this.nextSibling.classList.toggle("select-hide");
+            this.classList.toggle("select-arrow-active");
+        });
+    }
+}
+
+function createOptionElement(optionsList, selectElement, j) {
+    var optionElement = document.createElement("div");
+    optionElement.innerHTML = selectElement.options[j].innerHTML;
+    optionElement.addEventListener("click", function(e) {
+        var parentSelect, currentSelected, sameAsSelected;
+        parentSelect = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        currentSelected = this.parentNode.previousSibling;
+        for (let i = 0; i < parentSelect.length ; i++) {
+            if (parentSelect.options[i].innerHTML === this.innerHTML) {
+                parentSelect.selectedIndex = i;
+                currentSelected.innerHTML = this.innerHTML;
+                sameAsSelected = this.parentNode.getElementsByClassName("same-as-selected");
+                for ( let j = 0; j < sameAsSelected.length ; j++ ) {
+                    sameAsSelected[j].classList.remove("same-as-selected");
+                }
+                this.classList.add("same-as-selected");
+                break;
+            }
+        }
+        currentSelected.click();
+    });
+    optionsList.appendChild(optionElement);
+}
+// Custom select inputs
+
+
+function closeAllSelect(currentSelected) {
+    var allOptionsLists, allSelects, closeCurrentList = [];
+    allOptionsLists = $(".select-items");
+    allSelects = $(".select-selected");
+    for (let i = 0; i < allSelects.length ; i ++) {
+        if (currentSelected == allSelects[i]) {
+            closeCurrentList.push(i);
+        } else {
+            allSelects[i].classList.remove("select-arrow-active");
+        }
+    }
+    for ( let i = 0; i < allOptionsLists.length ; i ++) {
+        if (closeCurrentList.indexOf(i)) {
+            allOptionsLists[i].classList.add("select-hide");
+        }
+    }
+}
+
 $(document).ready(function() {
-    show();
+    createCustomSelects();
+    initializeCustomSelects();
+    document.addEventListener("click", closeAllSelect);
     $(".expensewrap, .incomewrap").on("click", function(e) {
         const id = findWrapId(e);
         const itemWrapWithInputs = $("#" + id)[0];
@@ -13,81 +102,3 @@ $(document).ready(function() {
     })
     $("#modalsavebutton").on("click", saveFromModalToItem)
 });
-
-//wrapping all selects in custom select divs
-var allSelects = $("select");
-for (let i = 0; i < allSelects.length ; i++) {
-    var parent = allSelects[i].parentNode;
-    var wrapper = document.createElement("div");
-    wrapper.setAttribute("class", "custom-select");
-    parent.replaceChild(wrapper, allSelects[i]);
-    wrapper.appendChild(allSelects[i]);
-    
-}
-
-// Custom select inputs
-var customSelects, selectElement, optionsLenght, currentSelectedOption, optionsList, optionElement;
-
-customSelects = $(".custom-select");
-for (let i = 0 ; i < customSelects.length ; i++) {
-    // create current selected item
-    selectElement = customSelects[i].getElementsByTagName("select")[0];
-    optionsLenght = selectElement.length;
-    currentSelectedOption = document.createElement("div");
-    currentSelectedOption.setAttribute("class", "select-selected");
-    currentSelectedOption.innerHTML = selectElement.options[selectElement.selectedIndex].innerHTML;
-    customSelects[i].appendChild(currentSelectedOption);
-    
-    optionsList = document.createElement("div");
-    optionsList.setAttribute("class", "select-items select-hide");
-    for (let j = 0 ; j < optionsLenght ; j++) {
-        optionElement = document.createElement("div");
-        optionElement.innerHTML = selectElement.options[j].innerHTML;
-        optionElement.addEventListener("click", function(e) {
-            var parentSelect, currentSelected, sameAsSelected;
-            parentSelect = this.parentNode.parentNode.getElementsByTagName("select")[0];
-            currentSelected = this.parentNode.previousSibling;
-            for (let i = 0; i < parentSelect.length ; i++) {
-                if (parentSelect.options[i].innerHTML === this.innerHTML) {
-                    parentSelect.selectedIndex = i;
-                    currentSelected.innerHTML = this.innerHTML;
-                    sameAsSelected = this.parentNode.getElementsByClassName("same-as-selected");
-                    for ( let j = 0; j < sameAsSelected.length ; j++ ) {
-                        sameAsSelected[j].classList.remove("same-as-selected");
-                    }
-                    this.classList.add("same-as-selected");
-                    break;
-                }
-            }
-            currentSelected.click();
-        });
-        optionsList.appendChild(optionElement);
-    }
-    customSelects[i].appendChild(optionsList);
-    currentSelectedOption.addEventListener("click", function(e) {
-        closeAllSelect(this);
-        this.nextSibling.classList.toggle("select-hide");
-        this.classList.toggle("select-arrow-active");
-    });
-}
-
-function closeAllSelect(currentSelected) {
-    var allOptionsLists, allClickedSelecteds, closeCurrentList = [];
-    allOptionsLists = $(".select-items");
-    allClickedSelecteds = $(".select-selected");
-    for (let i = 0; i < currentSelected.length ; i ++) {
-        if (currentSelected == allClickedSelecteds[i]) {
-            closeCurrent.push(i);
-        } else {
-            allClickedSelecteds[i].classList.remove("select-arrow-active");
-        }
-    }
-    for ( let i = 0; i < allOptionsLists ; i ++) {
-        if (closeCurrentList.indexOf(i)) {
-            allOptionsLists[i].classList.add("select-hide");
-        }
-    }
-}
-
-
-document.addEventListener("click", closeAllSelect);
--- readmes/README.de.md
+++ readmes/README.de.md
@@ -16,6 +16,56 @@ Die Hindernisse, die ich wahrend der Entwlickung der Projekt gefunden hatte find
 
 Die Dokumentation unsere APIs mit Swagger entwickelt war, um seinen Funktionen zu erklaren.
 
+### Uber das Projekt:
+
+# Das Dateibank, und was kommst am Nächsten
+
+Jedesmal du ein neues Item in deinem Konto erstellst, neue “Stats” sind also gerstellten.
+
+Gerade in diesem Fall ich habe ein implementation mit PostgreSQL in GitHub noch nicht gesehen, und es ist verständlich, denn est ist schwierig mit RDBMS Dateien durch Beziehungen zu verbinden. 
+
+Ich versuchte es, Datapoints  zu erstellen, aber es ging nicht, Datapoints sind die Stats eines Konto am ein Zeitpunkt. Nichts mehr als “Mein Ersparnisse für Mai 15, 2024” ich nenne Datapoints als “Dp” von nun an.
+
+z.B, Stats sind grundsätzlich einfach, es hatte nur drei Felder: totalIncomes, totalExpenses, totalSavings. Es gibt ein “Stats” für jedes Currency (USD, EUR, GBP) also drei Stats für jedes “Dp”. Aber wie viel Euro ein Dolar kosten änderst sich mit der Zeit.
+
+Also es ist bei ein BaseRatio gerechnet, der Base ist das Dolar und der Ratio wie viel Dolar ein andere Münze kostet (ein außer API gibst uns der aktuell BaseRatio) zum Beispiel von USD zu GBP es konnte 0.71 sein.
+
+Also vorstellt man, jedes POST Anfrage ein neues “Dp” für LocalDateTime.now() aufbauen --Also 1 Tabelle— wer drei (3) Stats hat —2 Tabelle—, jeder mit ein BaseRatio verbunden —3 Tabelle-. Es ist möglich, aber unnötig schwierig und der Absicht des Projekt nicht ein Komplexe Business Logic zu erstellen, sondern der Struktur den Microdienste zu erklären ist. Für ein potenter Beispiel für Business Logic mit RDBMS kannst du mein dmblog anzuschauen. Für dieses Grund habe ich ein Änderung zu ein MongoDB geplant. 
+
+# Das Gateway: Spring mit Thymeleaf
+
+Ich möchte ein tolles Interface erstellen sowie piggymetrics getan hat, aber ich habe Thymeleaf benutzt um es aufzubauen.
+
+GET request: Get das Konto von AccountService und zeigen es auf in index.html
+
+( Wenn das Konto noch nicht existiert, das Gateway erstellt es für dich mit Standardwerten )
+
+PUT: Formular mit versteckten Inputs, es gibt zwei Formulare, ein um das Kontodetails zu ändern, anderes für den Items zu arbeiten.
+
+DELETE: Ein knopf um das Konto zu entfernen
+
+Notiz: Selbst wenn du nicht ein Konto erstellen möchtest, kannst einfach als ‘demo’ fortfahren, es werde nur offenen endpoint anfragen.
+
+# Dev: H2 Dateibank, Prod: PostgreSQL Dateibank
+
+H2 Dateibank für jeder Dienst im Dev Profile, ein schnelle Notiz: wenn man das H2 mit ein vorgegebenes Nutzer für AuthServer nutzen, es ist nicht möglich das Login zu machen, weil das AuthServer sein eigenes BCryptEncoder nutzt um der Nutzer zu identifizieren, und es wird in keinem Fall mit der vorgegebene Passwort passen. 
+
+Dafür ich hatte ein Bean benutzt, um ein POST zu der /register Endpoint zu machen als bald als das App gestartet ist, sodass H2 hat ein demo Benutzer, womit man Login machen kann
+
+# Hindernisse:
+
+Eine meine schwieriger Hindernisse was der Dienste durch Eureka in Docker zu kommunizieren.
+
+Hindernis:
+
+Erst wenn man nur “http://localhost:9000/api/uaa” als Issuer jedes ResourceServer konfiguriert, der Docker Container kann nicht der AuthServer herausfinden.
+
+Lösung: Das Issuer IP Addresse durch DiscoveryClient finden, und als Bean in unsere Sicherheitseinstellungen anwen
+
+[Die Dockerfile](https://www.notion.so/Die-Dockerfile-1e47b6e81f244145b64b3458365b08ba?pvs=21)
+
+Select * From notifications u where u.active = true AND u.lastNotified <
+
 ### DIENSTE:
 ##### GATEWAY DIENST:
 
